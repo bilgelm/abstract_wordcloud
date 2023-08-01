0d845b5e8a5382718c5503fc741d19441b402a17
Murat Bilgel <bilgelm@users.noreply.github.com>
2023-08-01 10:48:22 -0400

    lint code; update r version; remove ipynb

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Abstract Wordcloud
 
-Jupyter+R: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/bilgelm/abstract_wordcloud/master)
-
 RStudio: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/bilgelm/abstract_wordcloud/master?urlpath=rstudio)
 
 Search PubMed for abstracts with a given author name and
--- abstract_wordcloud.r
+++ abstract_wordcloud.r
@@ -1,8 +1,19 @@
-
 # install packages (if necessary)
-list.of.packages <- c("RISmed","tm","SnowballC","wordcloud2","webshot","htmlwidgets","IRdisplay")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+list_of_packages <- c(
+  "RISmed",
+  "tm",
+  "SnowballC",
+  "wordcloud2",
+  "webshot",
+  "htmlwidgets",
+  "IRdisplay"
+)
+
+new_packages <- list_of_packages[
+  !(list_of_packages %in% installed.packages()[, "Package"])
+]
+
+if (length(new_packages)) install.packages(new_packages)
 
 webshot::install_phantomjs()
 
@@ -31,20 +42,25 @@ my_query <- "Murat Bilgel[AU]"
 
 # get pubmed data
 search_query <- EUtilsSummary(my_query)
-records<- EUtilsGet(search_query)
-pubmed_data <- data.frame('Title'=ArticleTitle(records),'Abstract'=AbstractText(records))
-head(pubmed_data, 2) # take a look at the first two results
+records <- EUtilsGet(search_query)
+pubmed_data <- data.frame(
+  "Title" = ArticleTitle(records),
+  "Abstract" = AbstractText(records)
+)
+# take a look at the first two results
+head(pubmed_data, 2)
 
 # get abstract body only
 docs <- VCorpus(VectorSource(pubmed_data$Abstract))
 
-lapply(docs, inspect) # take a look at the results
+# take a look at the results
+lapply(docs, inspect)
 
 # get rid of unnecessary characters in text
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
+to_space <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+docs <- tm_map(docs, to_space, "/")
+docs <- tm_map(docs, to_space, "@")
+docs <- tm_map(docs, to_space, "\\|")
 
 # Convert the text to lower case
 docs <- tm_map(docs, content_transformer(tolower))
@@ -53,35 +69,35 @@ docs <- tm_map(docs, removeNumbers)
 # Remove english common stopwords (is, are, was, at, some, ...)
 docs <- tm_map(docs, removeWords, stopwords("english"))
 # Remove additional words
-docs <- tm_map(docs, removeWords, c("abstract","abstracttext","objective","purpose","introduction","background",
-                                    "method","methods","material","materials",
-                                    "discussion","conclusion","conclusions",
-                                    "reference","references","bibliography",
-                                    "however")) 
+docs <- tm_map(
+  docs, removeWords,
+  c(
+    "abstract", "abstracttext", "objective", "purpose",
+    "introduction", "background", "method", "methods", "material",
+    "materials", "discussion", "conclusion", "conclusions",
+    "reference", "references", "bibliography",
+    "however", "can", "may"
+  )
+)
 # Remove punctuations
 docs <- tm_map(docs, removePunctuation)
 # Eliminate extra white spaces
 docs <- tm_map(docs, stripWhitespace)
-# Text stemming (i.e., get rid of suffixes)
-# docs <- tm_map(docs, stemDocument)
 
 # generate word frequency table
 dtm <- TermDocumentMatrix(docs)
 m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = names(v),freq=v)
+v <- sort(rowSums(m), decreasing = TRUE)
+d <- data.frame(word = names(v), freq = v)
 head(d, 20) # look at top 20 words
 
 # plot wordcloud
-# due to lack of htmlwidget support in jupyter notebook, we cannot display this directly here. 
-# We'll first save it as an interactive HTML, then export as png.
-mywordcloud = wordcloud2(d, size=1.0, color='random-dark')
+mywordcloud <- wordcloud2(d, size = 1.0, color = "random-dark")
+mywordcloud
 
-saveWidget(mywordcloud, "mywordcloud.html", selfcontained = F)
+# first save wordcloud as an interactive HTML, then export as png
+saveWidget(mywordcloud, "mywordcloud.html", selfcontained = FALSE)
 
 # a longer delay might be necessary to capture more words
-webshot("mywordcloud.html","mywordcloud.png", delay=60, vwidth=1000, vheight=1000)
-
-display_png(file="mywordcloud.png")
-
-
+webshot("mywordcloud.html", "mywordcloud.png", delay = 60,
+        vwidth = 1000, vheight = 1000)
