1d1be7f0dfe0353bdd72f3e8c07af83cd3f562c3
Murat Bilgel <bilgelm@users.noreply.github.com>
2023-08-04 22:57:10 -0400

    fix dependencies

--- abstract_wordcloud.r
+++ abstract_wordcloud.r
@@ -30,7 +30,6 @@ library(wordcloud2)
 # figure save and display
 library(webshot)
 library(htmlwidgets)
-library(IRdisplay)
 
 # Code put together by Murat Bilgel from:
 # http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
